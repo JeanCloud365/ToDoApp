8e8992f53e5740a6324aa58286ded50062f1aae8
JanVanMeirvenne <jan.vanmeirvenne@dexmach.com>
2020-04-07 10:23:47 +0200

    update

--- src/ToDoApp.UI/Startup.cs
+++ src/ToDoApp.UI/Startup.cs
@@ -131,7 +131,7 @@ namespace ToDoApp.UI
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+          //  app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
